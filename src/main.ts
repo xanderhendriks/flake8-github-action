fe81da79d27478bb423b2481cef117eab74a37d1
Xander Hendriks <xander@nx-solutions.com>
2024-01-21 17:01:04 +1100

    Updated to the latest typescript-action-example template and modified source to try to get it to work.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import * as github from '@actions/github';
-import * as octokit from '@octokit/rest';
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
 
-const { GITHUB_TOKEN } = process.env;
+const { GITHUB_TOKEN } = process.env
 
 async function runFlake8() {
-  let myOutput = '';
+  let myOutput = ''
   let options = {
     listeners: {
       stdout: (data: Buffer) => {
-        myOutput += data.toString();
-      },
+        myOutput += data.toString()
+      }
     }
-  };
-  await exec.exec('flake8 --exit-zero', [], options);
-  return myOutput;
+  }
+  await exec.exec('flake8 --exit-zero', [], options)
+  return myOutput
 }
 
-type Annotation = octokit.ChecksUpdateParamsOutputAnnotations;
+type ChecksUpdateParamsOutputAnnotations = {
+  path: string
+  start_line: number
+  end_line: number
+  start_column?: number
+  end_column?: number
+  annotation_level: 'notice' | 'warning' | 'failure'
+  message: string
+  title?: string
+  raw_details?: string
+}
+
+type Annotation = ChecksUpdateParamsOutputAnnotations
 // Regex the output for error lines, then format them in
 function parseFlake8Output(output: string): Annotation[] {
   // Group 0: whole match
@@ -27,18 +38,18 @@ function parseFlake8Output(output: string): Annotation[] {
   // Group 3: column number
   // Group 4: error code
   // Group 5: error description
-  let regex = new RegExp(/^(.*?):(\d+):(\d+): (\w\d+) ([\s|\w]*)/);
-  let errors = output.split('\n');
-  let annotations: Annotation[] = [];
+  let regex = new RegExp(/^(.*?):(\d+):(\d+): (\w\d+) ([\s|\w]*)/)
+  let errors = output.split('\n')
+  let annotations: Annotation[] = []
   for (let i = 0; i < errors.length; i++) {
-    let error = errors[i];
-    let match = error.match(regex);
+    let error = errors[i]
+    let match = error.match(regex)
     if (match) {
       // Chop `./` off the front so that Github will recognize the file path
-      const normalized_path = match[1].replace('./', '');
-      const line = parseInt(match[2]);
-      const column = parseInt(match[3]);
-      const annotation_level = <const> 'failure';
+      const normalized_path = match[1].replace('./', '')
+      const line = parseInt(match[2])
+      const column = parseInt(match[3])
+      const annotation_level = <const>'failure'
       const annotation = {
         path: normalized_path,
         start_line: line,
@@ -46,26 +57,30 @@ function parseFlake8Output(output: string): Annotation[] {
         start_column: column,
         end_column: column,
         annotation_level,
-        message: `[${match[4]}] ${match[5]}`,
-      };
+        message: `[${match[4]}] ${match[5]}`
+      }
 
-      annotations.push(annotation);
+      annotations.push(annotation)
     }
   }
-  return annotations;
+  return annotations
 }
 
-async function createCheck(check_name: string, title: string, annotations: Annotation[]) {
-  const octokit = new github.GitHub(String(GITHUB_TOKEN));
-  const res = await octokit.checks.listForRef({
+async function createCheck(
+  check_name: string,
+  title: string,
+  annotations: Annotation[]
+) {
+  const octokit = github.getOctokit(String(GITHUB_TOKEN))
+  const res = await octokit.rest.checks.listForRef({
     check_name,
     ...github.context.repo,
     ref: github.context.sha
-  });
+  })
 
-  const check_run_id = res.data.check_runs[0].id;
+  const check_run_id = res.data.check_runs[0].id
 
-  await octokit.checks.update({
+  await octokit.rest.checks.update({
     ...github.context.repo,
     check_run_id,
     output: {
@@ -73,23 +88,24 @@ async function createCheck(check_name: string, title: string, annotations: Annot
       summary: `${annotations.length} errors(s) found`,
       annotations
     }
-  });
+  })
 }
 
-async function run() {
+export async function run() {
   try {
-    const flake8Output = await runFlake8();
-    const annotations = parseFlake8Output(flake8Output);
+    const flake8Output = await runFlake8()
+    const annotations = parseFlake8Output(flake8Output)
     if (annotations.length > 0) {
-      console.log(annotations);
-      const checkName = core.getInput('checkName');
-      await createCheck(checkName, "flake8 failure", annotations);
-      core.setFailed(`${annotations.length} errors(s) found`);
+      console.log(annotations)
+      const checkName = core.getInput('checkName')
+      await createCheck(checkName, 'flake8 failure', annotations)
+      core.setFailed(`${annotations.length} errors(s) found`)
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed(`An unexpected error occurred: ${String(error)}`)
     }
   }
-  catch (error) {
-    core.setFailed(error.message);
-  }
 }
-
-run();
